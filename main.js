6c4342e4e4fd0c7fb25c8564add3ea1ef764eb23
Akram-HANY <ibnhany595@gmail.com>
2025-01-17 21:03:45 +0200

    Add files via upload
    Back To Top

--- main.js
+++ main.js
@@ -16,16 +16,30 @@ var all_products_json;
 var items_in_cart = document.querySelector(".items_in_cart");
 let product_cart = [];
 
-function addToCart(id,btn){
+function addToCart(id, btn) {
     product_cart.push(all_products_json[id])
     btn.classList.add("active")
     console.log(product_cart);
     getCartItems()
 }
-function getCartItems(){
+
+
+
+let count_item = document.querySelector(".count_item");
+let count_item_cart = document.querySelector(".count_item_cart");
+let price_cart_total = document.querySelector(".price_cart_total");
+
+
+let price_cart_Head = document.querySelector(".price_cart_Head");  
+
+
+
+function getCartItems() {
+    let total_price = 0;
+
     let items_c = "";
     for (let i = 0; i < product_cart.length; i++) {
-            items_c +=`
+        items_c += `
             
             <div class="item_cart">
                 <img src="${product_cart[i].img}" alt="">
@@ -37,21 +51,45 @@ function getCartItems(){
                 </div>
                 <button onclick = "remove_from_cart(${i})" class="delete_item"><i class="bi bi-trash-fill"></i></button>
              </div>
-            `     
-              
+       
+          `
+
+          total_price += product_cart[i].price
+
     }
 
     items_in_cart.innerHTML = items_c
 
+    price_cart_Head.innerHTML = "$" + total_price
+
+    count_item.innerHTML = product_cart.length
+    count_item_cart.innerHTML = `(${product_cart.length}Item in Cart)`
+    price_cart_total.innerHTML = "$" + total_price
+
 }
-   
-function remove_from_cart(index){
-    product_cart.splice(index,1)
+
+function remove_from_cart(index) {
+    product_cart.splice(index, 1)
     getCartItems()
-    
-     addToCartButtons = document.querySelectorAll(".bi-cart-plus");
+
+    addToCartButtons = document.querySelectorAll(".bi-cart-plus");
     for (let i = 0; i < addToCartButtons.length; i++) {
-        addToCartButtons[i].classList.remove("active") 
+        addToCartButtons[i].classList.remove("active")
         // adde just a comment       
     }
 }
+
+
+
+/*count item*/
+
+// back to top js//
+
+let back_to_top = document.querySelector(".back_to_top");
+
+back_to_top.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
